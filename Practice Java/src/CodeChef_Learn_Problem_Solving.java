3ef56d8891d4f4815e1003e58bf4d2244ec12b1c
Aditya Prakash <prakashaditya2209@gmail.com>
2023-09-14 15:42:48 +0530

    14-09-2023

---
+++ Practice Java/src/CodeChef_Learn_Problem_Solving.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class CodeChef_Learn_Problem_Solving {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() * 2);
+    }
+}
